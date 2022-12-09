44c0a606846694aced532ee80e4ab54ea64daaa5
Zavier Divelbiss <semiviral@gmail.com>
2022-12-09 01:04:17 -0600

    move lexer to bare module

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,4 @@
+use chumsky::Stream;
 use intaglio::Symbol;
 use logos::{Lexer, Logos};
 
@@ -120,16 +121,89 @@ pub enum TokenKind {
     Unknown,
 }
 
-fn trim_and_cache(lexer: &mut Lexer<TokenKind>) -> Option<Symbol> {
-    Some(crate::strings::intern_str(
+fn trim_and_cache(lexer: &mut Lexer<TokenKind>) -> Symbol {
+    crate::strings::intern_str(
         lexer
             .slice()
             .trim_start_matches('$')
             .trim_start_matches('"')
             .trim_end_matches('"'),
-    ))
+    )
 }
 
 fn parse_neg_integer(lexer: &mut Lexer<TokenKind>) -> Option<isize> {
     lexer.slice().replace('!', "-").as_str().parse().ok()
+}
+
+pub type Token = (TokenKind, logos::Span);
+
+pub struct TokenSource<'a> {
+    pub src: &'a str,
+    pub row: usize,
+    pub col: usize,
+}
+
+struct Tokenizer<'a>(logos::SpannedIter<'a, TokenKind>);
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let (kind, span) = self.0.next()?;
+
+        Some((kind, span))
+    }
+}
+
+pub struct TokenIterator<'a> {
+    src: &'a str,
+    lexer: std::iter::Peekable<Tokenizer<'a>>,
+}
+
+impl TokenIterator<'_> {
+    pub fn src(&self) -> &str {
+        self.src
+    }
+
+    pub fn peek(&mut self) -> Option<&Token> {
+        self.lexer.peek()
+    }
+
+    pub fn find_token(&self, (_, span): &Token) -> Option<TokenSource> {
+        let span_str = self.src.get(..span.end)?;
+        let (row, end_col) = span_str
+            .lines()
+            .enumerate()
+            .last()
+            .map(|(row, line_str)| (row, line_str.chars().count()))?;
+
+        Some(TokenSource {
+            src: span_str.get(span.start..)?,
+            row,
+            col: end_col - span.len(),
+        })
+    }
+}
+
+impl Iterator for TokenIterator<'_> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.lexer.next()
+    }
+}
+
+pub type Span = core::ops::Range<usize>;
+
+impl<'a> From<TokenIterator<'a>> for Stream<'a, TokenKind, Span, TokenIterator<'a>> {
+    fn from(lexer: TokenIterator<'a>) -> Self {
+        #[allow(clippy::range_plus_one)]
+        Self::from_iter(lexer.src().len()..(lexer.src().len() + 1), lexer)
+    }
+}
+
+pub fn lexer(input: &str) -> TokenIterator {
+    TokenIterator {
+        src: input,
+        lexer: Tokenizer(TokenKind::lexer(input).spanned()).peekable(),
+    }
 }
