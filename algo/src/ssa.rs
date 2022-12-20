4cdf503e428991490b22b644e4a3945c17bbf570
Zavier Divelbiss <semiviral@gmail.com>
2022-12-19 19:02:34 -0600

    fix ssa translation

--- algo/src/ssa.rs
+++ algo/src/ssa.rs
@@ -1,50 +1,88 @@
-use chumsky::chain::Chain;
-
 use crate::{
-    parser::{Expression, SpannedExpr},
+    parser::{Expression, HeapExpr, SpannedExpr},
     strings::Symbol,
     types::Type,
     Error, Operator, Primitive,
 };
 use std::collections::BTreeMap;
 
-type Scope = BTreeMap<Symbol, Binding>;
-type Scopes = Vec<BTreeMap<Symbol, Binding>>;
+type Scope = Vec<(Symbol, Binding)>;
+type Defs = BTreeMap<Symbol, (Type, SpannedExpr)>;
 
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Binding(usize);
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Instruction {
+pub enum Node {
     Bind(Primitive),
     Add(Binding, Binding),
     Sub(Binding, Binding),
     Jump(Binding),
+
+    Parameter(Type),
+    Expression { params: Binding, scope: Scope },
     Call(Binding),
 }
 
-pub fn translate(
-    tle: &SpannedExpr,
-    defs: Vec<crate::parser::HeapExpr>,
-) -> Result<(Vec<Instruction>, Scopes), Error> {
-    let mut nodes = Vec::new();
-    let mut scopes = vec![Scope::new()];
-    let defs = defs
-        .into_iter()
-        .map(|expr| {
-            let (expr, span) = *expr;
-            let Expression::VarDef { name, in_ty, expr } = expr
-            else { panic!("invalid definition type"); };
+fn stratify_exprs(ast: Vec<HeapExpr>) -> Result<(SpannedExpr, Defs), Vec<Error>> {
+    // TLE, or `Top Level Expression`
+    let (defs, tles) =
+        ast.into_iter()
+            .fold((Defs::new(), Vec::new()), |(mut defs, mut tles), expr| {
+                let (expr, span) = *expr;
+                match expr {
+                    Expression::VarDef { name, in_ty, expr } => {
+                        assert!(defs.insert(name, (in_ty, *expr)).is_none());
+                    }
 
-            (name, *expr)
-        })
-        .collect::<BTreeMap<Symbol, SpannedExpr>>();
+                    expr => {
+                        if !expr.is_def() {
+                            tles.push((expr, span))
+                        } else {
+                            panic!("failed to stratify all def expressions")
+                        }
+                    }
+                }
 
-    bind_expr(tle, &mut nodes, &mut scopes, &defs).map(|_| (nodes, scopes))
+                (defs, tles)
+            });
+
+    let mut tles_iter = tles.into_iter();
+    let tle = {
+        match tles_iter.len() {
+            1 => tles_iter.next().unwrap(),
+            0 => return Err(vec![Error::no_top_level_expr()]),
+            _ => {
+                return Err(tles_iter
+                    .map(|(_, span)| {
+                        Error::general(
+                            span.clone(),
+                            "cannot have multiple top-level expressions",
+                            Some("parse_input.tle_check"),
+                        )
+                    })
+                    .collect())
+            }
+        }
+    };
+
+    Ok((tle, defs))
 }
 
-fn bind_push(nodes: &mut Vec<Instruction>, new_node: Instruction) -> Binding {
+pub fn translate(ast: Vec<HeapExpr>) -> Result<(Vec<Node>, Scope), Vec<Error>> {
+    let (tle, mut defs) = stratify_exprs(ast)?;
+
+    let mut nodes = Vec::new();
+    let mut scope = Scope::new();
+
+    match bind_expr(&tle, &mut nodes, &mut scope, &mut defs) {
+        Ok(_) => Ok((nodes, scope)),
+        Err(err) => Err(vec![err]),
+    }
+}
+
+fn bind_push(nodes: &mut Vec<Node>, new_node: Node) -> Binding {
     let binding = Binding(nodes.len());
     nodes.push(new_node);
 
@@ -53,72 +91,131 @@ fn bind_push(nodes: &mut Vec<Instruction>, new_node: Instruction) -> Binding {
 
 fn bind_expr(
     expr: &SpannedExpr,
-    nodes: &mut Vec<Instruction>,
-    scopes: &mut Scopes,
-    defs: &BTreeMap<Symbol, SpannedExpr>,
+    nodes: &mut Vec<Node>,
+    scope: &mut Scope,
+    defs: &mut Defs,
 ) -> Result<Binding, Error> {
     match &expr.0 {
         Expression::Binary { lhs, op, rhs } => {
             // TODO uninitialized variable error
-            let lhs_binding = bind_expr(lhs.as_ref(), nodes, scopes, defs)?;
-            let rhs_binding = bind_expr(rhs.as_ref(), nodes, scopes, defs)?;
+            let lhs_binding = bind_expr(lhs.as_ref(), nodes, scope, defs)?;
+            let rhs_binding = bind_expr(rhs.as_ref(), nodes, scope, defs)?;
 
             match op {
-                Operator::Add => Ok(bind_push(nodes, Instruction::Add(lhs_binding, rhs_binding))),
-                Operator::Sub => Ok(bind_push(nodes, Instruction::Sub(lhs_binding, rhs_binding))),
+                Operator::Add => Ok(bind_push(nodes, Node::Add(lhs_binding, rhs_binding))),
+                Operator::Sub => Ok(bind_push(nodes, Node::Sub(lhs_binding, rhs_binding))),
 
                 _ => unimplemented!(),
             }
         }
 
-        Expression::Primitive(primitive) => {
-            Ok(bind_push(nodes, Instruction::Bind(primitive.clone())))
-        }
+        Expression::Primitive(primitive) => Ok(bind_push(nodes, Node::Bind(primitive.clone()))),
 
-        Expression::Identifier(symbol) => {
-            match scopes
-                .iter()
-                .rev()
-                .find_map(|scope| scope.get(symbol).copied())
-            {
-                Some(binding) => Ok(binding),
-
-                None => {
-                    let Some(expr) = defs.get(symbol)
-                    else {
-                        return Err(Error::undeclared_var(
-                            expr.1.clone(),
-                            crate::strings::get_intern_str(*symbol).as_str(),
-                            Some("ssa_identifier"),
-                        ));
-                    };
-
-                    bind_expr(expr, nodes, scopes, defs)
-                }
-            }
-        }
+        Expression::Identifier(symbol) => match find_binding(symbol, scope) {
+            Some(binding) => Ok(binding),
+            None => bind_def(symbol, nodes, defs),
+        },
 
         Expression::Flow {
             from: expr,
             to: next,
         } => {
-            scopes.push(Scope::new());
-            bind_expr(expr.as_ref(), nodes, scopes, defs)?;
-            if let Some(next) = next {
-                bind_expr(next, nodes, scopes, defs)?;
-            }
+            let bind_from = bind_expr(expr.as_ref(), nodes, scope, defs)?;
+            let bind_to = next
+                .as_ref()
+                .map(|expr| bind_expr(expr.as_ref(), nodes, scope, defs));
 
-            todo!()
+            bind_to.unwrap_or(Ok(bind_from))
         }
 
-        Expression::Control { exprs } => {
-            for expr in exprs {
-                bind_expr(expr, nodes, scopes, defs)?;
-            }
-
-            todo!()
-        }
+        Expression::Control { exprs } => exprs
+            .iter()
+            .map(|expr| bind_expr(expr, nodes, scope, defs))
+            .last()
+            .unwrap(),
 
         expr => unimplemented!("no SSA path: {:?}", expr),
+    }
+}
+
+fn bind_def(name: &Symbol, nodes: &mut Vec<Node>, defs: &mut Defs) -> Result<Binding, Error> {
+    let Some((ty, expr)) = defs.remove(name)
+                    else {
+                        return Err(Error::undeclared_var(
+                            0..0, // TODO
+                            crate::strings::get_intern_str(*name).as_str(),
+                            Some("ssa_identifier"),
+                        ));
+                    };
+
+    let mut def_scope = Scope::new();
+    let params = match ty {
+        Type::Unit => bind_push(nodes, Node::Parameter(Type::Unit)),
+        Type::Tuple(params) => params
+            .into_iter()
+            .map(|(name, ty)| {
+                let param_bind = bind_push(nodes, Node::Parameter(ty));
+                def_scope.push((name, param_bind));
+                param_bind
+            })
+            .last()
+            .unwrap(),
+
+        _ => unimplemented!(),
+    };
+
+    bind_expr(&expr, nodes, &mut def_scope, defs)?;
+    Ok(bind_push(
+        nodes,
+        Node::Expression {
+            params,
+            scope: def_scope,
+        },
+    ))
+}
+
+fn find_binding(symbol: &Symbol, scopes: &mut Scope) -> Option<Binding> {
+    scopes.iter().rev().find_map(|(other, binding)| {
+        if symbol.eq(other) {
+            Some(*binding)
+        } else {
+            None
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        interned,
+        ssa::{bind_def, stratify_exprs, Binding, Node},
+        types::Type,
+    };
+
+    #[test]
+    fn def_to_bindings() {
+        static SCRIPT: &str = "
+        var add_one: (a: Int) => a + 1;
+        1 => add_one
+        ";
+
+        let ast = crate::parser::parse(crate::lexer::lex(SCRIPT)).unwrap();
+        let (_, mut defs) = stratify_exprs(ast).unwrap();
+
+        let mut nodes = Vec::new();
+        let def = bind_def(&interned!("add_one"), &mut nodes, &mut defs).unwrap();
+        assert_eq!(def, Binding(3));
+        assert_eq!(
+            nodes,
+            vec![
+                Node::Parameter(Type::Int),
+                Node::Bind(crate::Primitive::Int(1)),
+                Node::Add(Binding(0), Binding(1)),
+                Node::Expression {
+                    params: Binding(0),
+                    scope: vec![(interned!("a"), Binding(0))]
+                }
+            ]
+        );
     }
 }
--- algo/src/types.rs
+++ algo/src/types.rs
@@ -1,14 +1,14 @@
 ///! Module defining everything related to the Algo type system.
 use crate::strings::Symbol;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Type {
     Unit, // is `()`
     Int,  // is `isize`
     UInt, // is `usize`
     Bool, // is `bool`
 
-    Tuple(Vec<(Option<Symbol>, Self)>),
+    Tuple(Vec<(Symbol, Self)>),
     Array { ty: Box<Self>, len: Option<usize> },
 
     Expression { input: Box<Self>, output: Box<Self> },
